0dd24699d2bd3de186979378f7c83c670243b69a
marko298 <boychuk.m.n@gmail.com>
2022-03-26 20:00:10 +0200

    less files

--- index.mjs
+++ index.mjs
@@ -1,6 +1,5 @@
-import { chunk, measureFn, range } from './utils.mjs';
-import { getBlock }                from './block.mjs';
-import { makeWeb3 }                from './web3.mjs';
+import { chunk, measureFn, range, retry } from './utils.mjs';
+import { makeWeb3 }                       from './web3.mjs';
 
 if (!process.argv[2]) {
   console.log('Usage: node index.mjs <node-url> [<parallelism>]');
@@ -8,12 +7,26 @@ if (!process.argv[2]) {
 }
 
 const web3 = makeWeb3(process.argv[2]);
+const parallelism = process.argv[3] || 10;
 
-const blocksToQuery = chunk(range(100_000, 110_000), +process.argv[3] || 10);
+export async function getBlock(number) {
+  const [block, traces, receipts] = await Promise.all([
+    retry(() => web3.eth.getBlock(number, true)),
+    retry(() => web3.eth.getBlockTracesParity(number)),
+    retry(() => web3.eth.getBlockReceiptsParity(number)),
+  ]);
+
+  block.traces = traces;
+  block.receipts = receipts;
+
+  return block;
+}
+
+const blocksToQuery = chunk(range(100_000, 110_000), parallelism);
 
 for (const blocks of blocksToQuery) {
   await measureFn(
     `Blocks ${Math.min(...blocks)}-${Math.max(...blocks)}`,
-    () => Promise.all(blocks.map((number) => getBlock(web3, number))),
+    () => Promise.all(blocks.map(getBlock)),
   );
 }
--- utils.mjs
+++ utils.mjs
@@ -1,3 +1,17 @@
+export async function retry(fn, count = 3) {
+  try {
+    return await fn();
+  } catch (e) {
+    if (count === 1) {
+      throw e;
+    }
+
+    console.log('Failure: ' + e.message);
+
+    return await retry(fn, count - 1);
+  }
+}
+
 export function range(from, to) {
   return Array.from({ length: to - from }, (_, i) => from + i);
 }
